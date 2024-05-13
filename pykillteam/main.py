73b4349cc94796405daf98d86ca1ed365d1d519a
Scott-Baldwin <superscottyb@gmail.com>
2024-05-13 00:26:20 -0600

    base model class

---
+++ pykillteam/main.py
@@ -0,0 +1,15 @@
+class Model:
+    def __init__(self) -> None:
+
+        self.name = None
+        self.faction = None
+
+        self.movement = None
+        self.action_point_limit = None
+        self.group_activation = None
+        self.defence = None
+        self.save = None
+        self.wounds = None
+
+        self.weapons = {}
+        self.equipment = {}
